233bed641538308c70e1ea191917b18ab752de79
splat31 <bastien31800@orange.fr>
2025-10-05 13:39:34 +0200

    init: lectureTSPLIB

---
+++ src/lectureTSPLIB.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "struct.h"
+#include "lectureTSPLIB.h"
+
+Graphe* read_TSPLIB (const char *fichier) {
+    exit (1);
+}
+
+int main() {
+    return 0;
+}
---
+++ src/lectureTSPLIB.h
@@ -0,0 +1,8 @@
+#ifndef STRUCTURE_H
+#define STRUCTURE_H
+
+#include "struct.h"
+
+Graphe* read_TSPLIB (const char *fichier);
+
+#endif
